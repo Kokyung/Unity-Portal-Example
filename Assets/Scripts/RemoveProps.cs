c729e9c70ee54ff816ab9331d985fac663da99d8
Kokyung <kikikg6@naver.com>
2020-02-28 15:37:53 +0900

    Minor Fix

---
+++ Assets/Scripts/RemoveProps.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveProps : MonoBehaviour
+{
+    public bool Show;
+    public GameObject[] gameObjects;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            for (int i = 0; i < gameObjects.Length; i++)
+            {
+                gameObjects[i].SetActive(Show);
+            }
+        }
+    }
+}
--- Assets/Scripts/portalTeleporter.cs
+++ Assets/Scripts/portalTeleporter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class portalTeleporter : MonoBehaviour
 {
-    public CharacterController player;
+    public GameObject Player;
     public Transform receiver;
 
     private bool playerOverlapping = false;
@@ -13,24 +13,27 @@ public class portalTeleporter : MonoBehaviour
     {
         if (playerOverlapping)
         {
-            Debug.Log($"Before Location: {player.transform.position}");
-            Vector3 portalToPlayer = player.transform.position - transform.position;
+            Debug.Log($"Before Location: {Player.transform.position}");
+            Vector3 portalToPlayer = Player.transform.position - transform.position;
             float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
             Debug.Log($"Dot: {dotProduct}");
-            if (dotProduct < 1f) // only transport the player once he's moved across plane
+            if (dotProduct < 0f) // only transport the player once he's moved across plane
             {
                 // transport him to the equivalent position in the other portal
                 float rotDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
                 rotDiff += 180;
-                player.transform.Rotate(Vector3.up, rotDiff);
+                Player.transform.Rotate(Vector3.up, rotDiff);
 
-                Vector3 positionOffset = Quaternion.Euler(0, rotDiff, 0) * portalToPlayer;
-                player.enabled = false;
-                player.transform.position = receiver.position + positionOffset;
-                player.enabled = true;
+                Vector3 positionOffset = Quaternion.Euler(0f, rotDiff, 0f) * portalToPlayer;
+
+                Player.GetComponent<CharacterController>().enabled = false;
+
+                Player.transform.position = receiver.position + positionOffset;
+
+                Player.GetComponent<CharacterController>().enabled = true;
 
                 playerOverlapping = false;
-                Debug.Log($"After Location: {player.transform.position}");
+                Debug.Log($"After Location: {Player.transform.position}");
             }
         }
     }
@@ -41,7 +44,7 @@ public class portalTeleporter : MonoBehaviour
         {
             receiver.GetComponent<BoxCollider>().enabled = false;
             playerOverlapping = true;
-            Debug.Log($"Enter Portal: {player.transform.position}");
+            Debug.Log($"Enter Portal: {Player.transform.position}");
         }
     }
     
@@ -50,7 +53,7 @@ public class portalTeleporter : MonoBehaviour
         if(other.tag == "Player")
         {
             playerOverlapping = false;
-            Debug.Log($"Exit Portal: {player.transform.position}");
+            Debug.Log($"Exit Portal: {Player.transform.position}");
         }
     }
     
